afbd1fdcaaba1bd82b38d30dbc93272122e2fc7c
Brian Anderson <banderson@mozilla.com>
2012-11-02 00:07:42 -0700

    Add VoidPtrLike trait for converting between *c_void

--- util.rs
+++ util.rs
@@ -1,6 +1,12 @@
 use ll::stylesheet::css_fixed;
+use core::libc::c_void;
 
 pub fn css_fixed_to_float(f: css_fixed) -> float {
     const before: i32 = 10;
     f as float * 1.0f / ((1i32 << before) as float)
+}
+
+pub trait VoidPtrLike {
+    static fn from_void_ptr(*c_void) -> self;
+    fn to_void_ptr(&self) -> *c_void;
 }
