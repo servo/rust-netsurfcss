0ffe9291baaf82a74d67df27fdf5169074551292
Jack Moffitt <jack@metajack.im>
2013-03-28 16:36:12 -0600

    Update to Rust 0.6 syntax.

--- util.rs
+++ util.rs
@@ -4,12 +4,12 @@ use core::libc::c_void;
 use lwcstr_from_rust_str = wapcaplet::from_rust_string;
 
 pub fn css_fixed_to_float(f: css_fixed) -> float {
-    const before: i32 = 10;
+    static before: i32 = 10;
     f as float * 1.0f / ((1i32 << before) as float)
 }
 
 pub fn float_to_css_fixed(f: float) -> css_fixed {
-    const before: i32 = 10;
+    static before: i32 = 10;
     (f * ((1 << before) as float)) as css_fixed
 }
 
@@ -20,13 +20,13 @@ pub fn rust_str_to_net_qname(s: &str) -> CssQName {
     }
 }
 
-pub fn net_qname_to_rust_str(qname: &a/CssQName) -> &a/str {
+pub fn net_qname_to_rust_str<'a>(qname: &'a CssQName) -> &'a str {
     qname.name.to_str_slice()
 }
 
 
 // FIXME: These methods should be unsafe
 pub trait VoidPtrLike {
-    static fn from_void_ptr(*c_void) -> Self;
+    fn from_void_ptr(*c_void) -> Self;
     fn to_void_ptr(&self) -> *c_void;
 }
