7d68385285a43691c3e341cdc3f7aad86a9b4236
Patrick Walton <pcwalton@mimiga.net>
2013-09-26 21:28:17 -0700

    Inline stuff and don't call through to the FFI for many properties.

--- util.rs
+++ util.rs
@@ -12,11 +12,13 @@ use types::CssQName;
 use std::libc::c_void;
 use lwcstr_from_rust_str = wapcaplet::from_rust_string;
 
+#[inline]
 pub fn css_fixed_to_float(f: css_fixed) -> float {
     static BEFORE: i32 = 10;
     f as float * 1.0f / ((1i32 << BEFORE) as float)
 }
 
+#[inline]
 pub fn float_to_css_fixed(f: float) -> css_fixed {
     static BEFORE: i32 = 10;
     (f * ((1 << BEFORE) as float)) as css_fixed
